7dc0f497a1a8f7cd501e0570c970abec3097cf45
Faheem Ahmad <fisforfaheem@gmail.com>
2021-08-07 17:57:18 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:ticknotes/pages/login.dart';
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
